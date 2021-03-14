e2e8f77451cc19aea9c3ad2bfb7c75dcb3aa6cf6
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-03-14 09:59:19 +0100

    dodano rozw zadania 4

---
+++ part1_13_03_2021/z4/MinMaxOfNums.hs
@@ -0,0 +1,27 @@
+-------------------------------------------------------------------------------
+--                              zmienne globalne                             --
+-------------------------------------------------------------------------------
+nums :: [Int]
+nums = [1, 6, 23, 8, 4, 98, 3, 7, 3, 98, 4, 98]
+
+
+-------------------------------------------------------------------------------
+--                                  funkcje                                  --
+-------------------------------------------------------------------------------
+-- w haskellu nie ma for-a, jest wiec problem z zasymulowaniem 1 loopa
+-- mozna oczywiscie dac cos w stylu
+
+-- zwraca tuple (min, max)
+minMaxOfNums1 :: [Int] -> (Int, Int)
+minMaxOfNums1 nums = (minimum nums, maximum nums)
+-- ale, ze to uzywa funcji wbudowanych ktore zwracaja minimum i maksimum z listy
+
+-- moze wiec cos w stylu
+-- (map|foldl|foldr wydaja sie byc odpowiednikami for-a dla roznych sytuacji)
+-- min i max to wbudowane operatory zwracajace min-a i max-a z 2 cyfr
+-- zwraca tuple (low, high)
+minMaxOfNums2 :: (Ord a) => [a] -> (a, a)
+-- foldl (\accCurVal curEltOfList -> newAccVal) accInitVal list
+minMaxOfNums2 (x:xs) = foldl
+  (\(low, high) curVal -> (min low curVal, max high curVal))
+  (x, x) xs
---
+++ part1_13_03_2021/z4/README.md
@@ -0,0 +1,34 @@
+# Info
+
+Tresci zadan skopiowane z tych ktore dostalismy od Karola na podyplomowki JS Developer 2020/2021.
+
+Zadania z dnia 17-10-2020
+
+W ramach treningu rozwiaze je w Haskellu.
+
+# Moj obecny background
+
+Przeczytane 6 pierwszych rozdzialow z [Learn You a Haskell for Great Good!](http://learnyouahaskell.com/chapters).
+
+Dalsze rozdzialy beda czytane/przerabiane w miare mozliwosci.
+
+# Task 4
+
+Choose highest and lowest values from the given array. [1,6,23,8,4,98,3,7,3,98,4,98]. One loop run.
+(chodzi chyba o uzycie for-a 1x, a nie o 1 iteracje/obrot for-a)
+
+# Uruchomienie
+
+Wejsc do pliku `MinMaxOfNums.hs` z Emacsa, i `C-c C-l`
+
+Po pojawieniu sie REPL-a wpisac:
+
+```
+desiredFunc [args]
+```
+
+i voila.
+
+# Wyjscie
+
+`M-x haskell-session-kill`
