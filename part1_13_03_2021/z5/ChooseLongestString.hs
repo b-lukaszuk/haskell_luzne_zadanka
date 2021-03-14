1b58b19765ae91a9876b5c6c36ab6ec2750d7269
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-03-14 10:39:33 +0100

    dodano rozw zad 5

---
+++ part1_13_03_2021/z5/ChooseLongestString.hs
@@ -0,0 +1,27 @@
+-------------------------------------------------------------------------------
+--                              zmienne globalne                             --
+-------------------------------------------------------------------------------
+lstOfWords :: [String]
+lstOfWords = [ "Politechnika1", "Karol", "Adam", "Rogowski",
+               "Politechnika2", "Super", "Weekend"]
+
+
+-------------------------------------------------------------------------------
+--                                  funkcje                                  --
+-------------------------------------------------------------------------------
+-- zwraca najdluzsze slowo (pierwsze od lewej) z listy
+getLongestWord :: [String] -> String
+getLongestWord lstOfWords = foldl1
+  (\acc w -> if length w > length acc then w else acc)
+  lstOfWords
+
+
+-- zwraca najdluzsze slowo (lub kilka rownie dlugich) z listy
+getLongestWords :: [String] -> [String]
+getLongestWords (w:ws)= foldl
+  (\acc curWord ->
+     if length curWord > length (acc !! 0) then [curWord]
+     else if length curWord == length (acc !! 0) then acc ++ [curWord]
+     else acc)
+  [""] lstOfWords
+
---
+++ part1_13_03_2021/z5/README.md
@@ -0,0 +1,35 @@
+# Info
+
+Tresci zadan skopiowane z tych ktore dostalismy od Karola na podyplomowki JS Developer 2020/2021.
+
+Zadania z dnia 17-10-2020
+
+W ramach treningu rozwiaze je w Haskellu.
+
+# Moj obecny background
+
+Przeczytane 6 pierwszych rozdzialow z [Learn You a Haskell for Great Good!](http://learnyouahaskell.com/chapters).
+
+Dalsze rozdzialy beda czytane/przerabiane w miare mozliwosci.
+
+# Task 5
+
+Choose longest string from the array. [‘Karol’, ‘Adam’,’Rogowski’,’Politechnika’,’Super’,’Weekend’].
+
+tylko 1, czy kilka w przypadku remisow?! (wezme kilka)
+
+# Uruchomienie
+
+Wejsc do pliku `ChooseLongestString.hs` z Emacsa, i `C-c C-l`
+
+Po pojawieniu sie REPL-a wpisac:
+
+```
+desiredFunc [args]
+```
+
+i voila.
+
+# Wyjscie
+
+`M-x haskell-session-kill`
