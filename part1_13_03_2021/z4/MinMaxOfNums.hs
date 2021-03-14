2ec76f6f0067aa279f63b7ed3c0e56bca5fea6d1
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-03-14 10:05:29 +0100

    zad4, kosmetyka

--- part1_13_03_2021/z4/MinMaxOfNums.hs
+++ part1_13_03_2021/z4/MinMaxOfNums.hs
@@ -14,10 +14,11 @@ nums = [1, 6, 23, 8, 4, 98, 3, 7, 3, 98, 4, 98]
 -- zwraca tuple (min, max)
 minMaxOfNums1 :: [Int] -> (Int, Int)
 minMaxOfNums1 nums = (minimum nums, maximum nums)
--- ale, ze to uzywa funcji wbudowanych ktore zwracaja minimum i maksimum z listy
+-- ale, to uzywa funcji wbudowanych ktore zwracaja minimum i maksimum z listy
+-- wiec chyba nie o to chodzi
 
--- moze wiec cos w stylu
 -- (map|foldl|foldr wydaja sie byc odpowiednikami for-a dla roznych sytuacji)
+-- moze wiec cos w stylu
 -- min i max to wbudowane operatory zwracajace min-a i max-a z 2 cyfr
 -- zwraca tuple (low, high)
 minMaxOfNums2 :: (Ord a) => [a] -> (a, a)
