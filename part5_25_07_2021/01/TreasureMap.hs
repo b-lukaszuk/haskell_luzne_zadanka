5b40b3323e703ed9858032f4e7d903da1f380f4d
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-07-25 22:27:50 +0200

    dodanie reprezentacje mapy skarbow i jej drukowania

---
+++ part5_25_07_2021/01/TreasureMap.hs
@@ -0,0 +1,50 @@
+module TreasureMap (treasureMap) where
+
+
+-------------------------------------------------------------------------------
+--                                  importy                                  --
+-------------------------------------------------------------------------------
+-- nothing to import here
+
+
+-------------------------------------------------------------------------------
+--                              zmienne globalne                             --
+-------------------------------------------------------------------------------
+treasureMap :: [[Int]]
+treasureMap = [
+  [34, 31, 32, 41, 25],
+  [14, 42, 43, 14, 31],
+  [54, 45, 52, 42, 23],
+  [33, 15, 51, 31, 35],
+  [21, 52, 33, 13, 23]]
+
+cellHorizBound :: String
+cellHorizBound = "----"
+
+
+-------------------------------------------------------------------------------
+--                                  funkcje                                  --
+-------------------------------------------------------------------------------
+padDigit :: Int -> String
+padDigit num = " " ++ (show num) ++ " "
+
+myConcatWith :: String -> [String] -> String
+myConcatWith symb [] = symb
+myConcatWith symb (x:xs) = symb ++ x ++ myConcatWith symb xs
+
+tmRowToString :: [Int] -> String
+tmRowToString [] = ""
+tmRowToString xs = myConcatWith "|" $ map padDigit xs
+
+getHorizSep :: [Int] -> String
+getHorizSep xs = myConcatWith "+" $ replicate (length xs) cellHorizBound
+
+tmToString' :: [[Int]] -> [String]
+tmToString' [] = []
+tmToString' (x:xs) = tmRowToString x : tmToString' xs
+
+tmToString :: [[Int]] -> String
+tmToString all@(x:xs) =
+  let sep = "\n" ++ (getHorizSep x) ++ "\n"
+      tresMap = tmToString' all
+  in myConcatWith sep tresMap
