63e7fafe17003f518ba7b18b4502145ef4af7777
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-07-30 11:57:00 +0200

    dodano rozw zadan 3.1 i 3.2

--- part5_25_07_2021/03/main.hs
+++ part5_25_07_2021/03/main.hs
@@ -1,11 +1,42 @@
 -------------------------------------------------------------------------------
 --                                  task 3.1                                 --
 -------------------------------------------------------------------------------
+safeHead :: [a] -> Maybe a
+safeHead [] = Nothing
+safeHead (x:xs) = Just x
+
+safeTail :: [a] -> Maybe [a]
+safeTail [] = Nothing
+safeTail (x:xs) = Just xs
+
+safeLast :: [a] -> Maybe a
+safeLast [] = Nothing
+safeLast (x:xs) = if null xs then Just x else safeLast xs
+
+safeInit :: [a] -> Maybe [a]
+safeInit [] = Nothing
+safeInit (x:xs) = Just xs
+
 
 -------------------------------------------------------------------------------
 --                                  task 3.2                                 --
 -------------------------------------------------------------------------------
+-- tail function similar to the one in common lisp
+clTail :: [a] -> [a]
+clTail [] = []
+clTail (x:xs) = xs
 
--------------------------------------------------------------------------------
+-- splits list at elements where predicate is/are False
+-- the stop/split elements do not belong to the result
+splitWith :: (a -> Bool) -> [a] -> [[a]]
+splitWith _ [] = []
+splitWith predicate xs =
+  let (begin, rest) = break predicate xs
+  in if null begin
+     then splitWith predicate (clTail rest)
+     else [begin] ++ splitWith predicate (clTail rest)
+
+
+------------------------------------------------------------------------------
 --                                  task 3.3                                 --
 -------------------------------------------------------------------------------
