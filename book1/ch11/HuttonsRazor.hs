069099c503b30a308b323847594a4963ef92141e
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-08-30 21:30:24 +0200

    kosmetyka

--- book1/ch11/HuttonsRazor.hs
+++ book1/ch11/HuttonsRazor.hs
@@ -4,10 +4,10 @@ data Expr = Lit Integer | Add Expr Expr
 
 eval :: Expr -> Integer
 eval (Lit x) = x
-eval (Add e1 e2) = (eval e1 ) + (eval e2)
+eval (Add e1 e2) = (eval e1) + (eval e2)
 
 -- hmm, is that it?
--- some, simple checks
+-- ok, some simple checks to verify that
 r1 :: Integer
 r1 = eval (Add (Lit 1) (Lit 9001))
 
