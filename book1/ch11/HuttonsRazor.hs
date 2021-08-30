f3a97824c6ee265468dfa67517a615be53a7c5fa
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-08-30 20:36:15 +0200

    dodano rozwiazanie do sekcji "Hutton's Razor"

---
+++ book1/ch11/HuttonsRazor.hs
@@ -0,0 +1,19 @@
+module HuttonsRazor where
+
+data Expr = Lit Integer | Add Expr Expr
+
+eval :: Expr -> Integer
+eval (Lit x) = x
+eval (Add e1 e2) = (eval e1 ) + (eval e2)
+
+-- hmm, is that it?
+-- some, simple checks
+r1 :: Integer
+r1 = eval (Add (Lit 1) (Lit 9001))
+
+r2 :: Integer
+r2 = eval (Add (Add (Lit 1) (Lit 4)) (Lit 9001))
+
+r3 :: Integer
+r3 = eval (Add (Add (Lit 1) (Lit 4)) (Add (Lit 100) (Lit 200)))
+-- it appears to be the end
