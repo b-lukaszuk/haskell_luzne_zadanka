e5865314c1e4b1a51ed8268f219059c7c5aa915c
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-09-06 15:49:34 +0200

    dodano scaffolding projektu 'Hangman' step1-step2 i pocz. step3

---
+++ book1/ch13/hangman/src/Main.hs
@@ -0,0 +1,66 @@
+module Main where
+
+import Control.Monad (forever)
+import Data.Char (toLower)
+import Data.Maybe (isJust)
+import Data.List (intersperse)
+import System.Exit (exitSuccess)
+import System.Random (randomRIO)
+
+type WordList = [String]
+
+allWords :: IO WordList
+allWords = do
+  dict <- readFile "data/dict.txt"
+  return (lines dict)
+
+minWordLength :: Int
+minWordLength = 5
+
+maxWordLength :: Int
+maxWordLength = 9
+
+isBetw :: Int -> Int -> Int -> Bool
+isBetw someNum minIncl maxIncl = someNum >= minIncl && someNum <= maxIncl
+
+gameWords :: IO WordList
+gameWords = do
+  aw <- allWords
+  return (filter (\aWrd -> isBetw (length aWrd) minWordLength maxWordLength) aw)
+
+randomWord :: WordList -> IO String
+randomWord wl = do
+  randomIndex <- randomRIO (0, (length wl) - 1)
+  return $ wl !! randomIndex
+
+randomWord' :: IO String
+randomWord' = gameWords >>= randomWord
+
+-- type synonyms (my modif)
+type WordToGuess = String
+type CharsFilledSoFar = [Maybe Char]
+type LettersGuessedSoFar = [Char]
+
+-- helper fn (my modif)
+getCharRepr :: Maybe Char -> Char
+getCharRepr Nothing = '_'
+getCharRepr (Just letter) = letter
+
+-- helper fn (my modif)
+getCharsRepr :: [Maybe Char] -> String
+getCharsRepr text  = map getCharRepr text
+
+data Puzzle = Puzzle WordToGuess CharsFilledSoFar LettersGuessedSoFar
+
+instance Show Puzzle where
+  show (Puzzle _ discovered guessed) =
+    (intersperse ' ' $ getCharsRepr discovered) ++
+    " Guessed so far: " ++ guessed
+
+test :: Puzzle
+test = (Puzzle "abc" [Just 'a', Nothing, Just 'b'] "kasic")
+
+
+main :: IO ()
+main = do
+  putStrLn "hello world"
