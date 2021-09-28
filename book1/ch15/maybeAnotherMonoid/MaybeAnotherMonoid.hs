7359143239fc81c00837dee367e493ecb16c48fe
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-09-28 16:54:11 +0200

    dodano instancje (Monoid, Semigroup) do typu Maybe'

--- book1/ch15/maybeAnotherMonoid/MaybeAnotherMonoid.hs
+++ book1/ch15/maybeAnotherMonoid/MaybeAnotherMonoid.hs
@@ -1,6 +1,7 @@
 module MaybeAnotherMonoid where
 
 import Data.Monoid
+import Test.QuickCheck
 
 -- code from the section 'Optional Monoid` in this chapter (ch 15)
 data Optional a = Nada | Only a deriving (Eq, Show)
@@ -14,21 +15,32 @@ instance Semigroup a => Semigroup (Optional a) where
   (Only x) <> (Only y) = Only (x <> y)
   Nada <> Nada = Nada
 
--- provided scaffolding
+-- provided scaffolding + my code
 newtype First' a = First' { getFirst' :: Optional a } deriving (Eq, Show)
 
-instance Monoid (First' a) where
-	mempty = undefined
-	mappend = undefined
+-- my solution, monoid for Maybe which doesn't require monoid for the contents
+data Maybe' a = Nothing' | Just' deriving (Eq, Show)
 
-firstMappend :: First' a -> First' a -> First' a
-firstMappend = mappend
+instance Monoid (Maybe' a) where
+  mempty = Nothing'
+
+instance Semigroup (Maybe' a) where
+  Nothing' <> _ = Nothing'
+  _ <> Nothing' = Nothing'
+  Just' <> Just' = Just'
+
+-- instance Monoid (First' a) where
+-- 	mempty = undefined
+-- 	mappend = undefined
+
+-- firstMappend :: First' a -> First' a -> First' a
+-- firstMappend = mappend
 
 type FirstMappend = First' String -> First' String -> First' String -> Bool
 type FstId = First' String -> Bool
 
-main :: IO ()
-main = do
-	quickCheck (monoidAssoc :: FirstMappend)
-	quickCheck (monoidLeftIdentity :: FstId)
-	quickCheck (monoidRightIdentity :: FstId)
+-- main :: IO ()
+-- main = do
+-- 	quickCheck (monoidAssoc :: FirstMappend)
+-- 	quickCheck (monoidLeftIdentity :: FstId)
+-- 	quickCheck (monoidRightIdentity :: FstId)
