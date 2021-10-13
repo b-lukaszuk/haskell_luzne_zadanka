3b28054dae7e1eba069ce20a45162514767c5abf
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-10-13 21:31:02 +0200

    dodano rozwiazanie do Task 5 (height of a tree)

--- book3/ch03/treeHeight.hs
+++ book3/ch03/treeHeight.hs
@@ -16,14 +16,33 @@ addBranch x (Node left y right) =
 mkBinTree :: (Ord a) => [a] -> Tree a
 mkBinTree xs = foldr (\x acc -> addBranch x acc) Empty xs
 
+-- each tree (t1..t6) should have the height equal to the digit in its name
+-- check it in ghci
+t0 :: Tree Int
+t0 = mkBinTree []
+
 t1 :: Tree Int
-t1 = mkBinTree []
+t1 = mkBinTree [1]
 
 t2 :: Tree Int
-t2 = mkBinTree [3, 2, 5]
+t2 = mkBinTree [6, 2, 5]
 
 t3 :: Tree Int
-t3 = mkBinTree [3, 2, 5, 6]
+t3 = mkBinTree [3, 2, 5]
 
 t4 :: Tree Int
 t4 = mkBinTree [3, 2, 5, 6, 1, 4]
+
+t5 :: Tree Int
+t5 = mkBinTree [1..5]
+
+t6 :: Tree Int
+t6 = mkBinTree [1..6]
+
+-- is that it? it seems so
+count :: Tree a -> Integer
+count Empty = 0
+count (Node left y right) = 1 + max (count left) (count right)
+
+getHeight :: Tree a -> Integer
+getHeight aTree = count aTree
