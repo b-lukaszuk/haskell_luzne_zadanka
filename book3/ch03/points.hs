d4916c7555ef56ffa9cba28813f536dfafb925da
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-10-17 22:17:13 +0200

    modyf wczesniejszego kodu (Task 6), dod. rozw. do Task 7

--- book3/ch03/points.hs
+++ book3/ch03/points.hs
@@ -1,40 +1,12 @@
 type X = Double
 type Y = Double
 
-data Point = Point (X, Y) deriving (Show)
+data Point = Point (X, Y)
 
-data Line = Line (Point, Point) deriving (Show)
+instance Show Point where
+  show (Point (x, y)) = "(" ++ show x ++ ", " ++ show y ++ ")"
 
--- https://www.youtube.com/watch?v=JSEPDJfl8m8
--- tangent theta = abs[(slope2-slope1)/1+slope2*slope1]
--- angleRad = tangent inverse of tangent theta
-
-slope :: Line -> Double
-slope (Line (Point (x1, y1), Point (x2, y2))) =
-  let deltaY = y2 - y1
-      deltaX = x2 - x1
-  in deltaY / deltaX
-
-tangentTheta :: Line -> Line -> Double
-tangentTheta l1 l2 =
-  let slope1 = slope l1
-      slope2 = slope l2
-      numerator = slope2 - slope1
-      denominator = 1 + slope2 * slope1
-  in abs $ numerator / denominator
-
--- returns angle in radians
-angleRad :: Line -> Line -> Double
-angleRad l1 l2 =
-  let tanTheta = tangentTheta l1 l2
-  in atan tanTheta
-
--- 1rad = 180deg/PI
-radToDeg :: Double -> Double
-radToDeg x = 180/pi * x
-
-angleDeg :: Line -> Line -> Double
-angleDeg l1 l2 = radToDeg $ angleRad l1 l2
+--https://algorithmtutor.com/Computational-Geometry/Determining-if-two-consecutive-segments-turn-left-or-right/
 
 p1 :: Point
 p1 = Point (0, 1)
@@ -43,10 +15,53 @@ p2 :: Point
 p2 = Point (1, 2)
 
 p3 :: Point
-p3 = Point (1.5, 3)
+p3 = Point (1, 3)
 
--- now we could use the angle to determine the turn of the line
--- assuming, e.g 0-90 right, -90-0 left, else Straight (NaN for Inifinite slope)
--- but that's enough for today, actually, I was only supposed to define
--- the type below
-data Direction = Left | Right | Straight
+p4 :: Point
+p4 = Point (4, 1)
+
+p5 :: Point
+p5 = Point (2, 1)
+
+p6 :: Point
+p6 = Point (2, 2)
+
+p7 :: Point
+p7 = Point (2, 3)
+
+-- if crossProd is positive then p1 i clockwise from p2, with respect to (0, 0)
+-- if crossProd is neg. then p1 i ccw from p2, with respect to (0, 0)
+-- if crossProd is 0 then they are colinear (lie on a lingle line)
+calcCrossProd :: Point -> Point -> Double
+calcCrossProd (Point (x1, y1)) (Point (x2, y2)) = x1*y2 - x2*y1
+
+ptsToVector :: Point -> Point -> Point
+ptsToVector (Point (x1, y1)) (Point (x2, y2)) = (Point (x2-x1, y2-y1))
+
+calcDirection :: Point -> Point -> Point -> Double
+calcDirection pt1 pt2 pt3 =
+  calcCrossProd (ptsToVector pt1 pt3) (ptsToVector pt1 pt2)
+
+data Direction = DirLeft | DirRight | DirStraight deriving (Show)
+
+getDirection :: Point -> Point -> Point -> Direction
+getDirection pt1 pt2 pt3 =
+  let dirVal = calcDirection pt1 pt2 pt3
+  in if dirVal < 0 then DirLeft
+  else if dirVal > 0 then DirRight
+  else DirStraight
+
+main :: IO()
+main = do
+  putStrLn "Testing directions between Points test points."
+  putStrLn ""
+  putStrLn $ "Direction: " ++ show p1 ++ ", " ++ show p2 ++ ", " ++ show p3
+  putStrLn $ show $ getDirection p1 p2 p3
+  putStrLn ""
+  putStrLn $ "Direction: " ++ show p1 ++ ", " ++ show p2 ++ ", " ++ show p4
+  putStrLn $ show $ getDirection p1 p2 p4
+  putStrLn ""
+  putStrLn $ "Direction: " ++ show p5 ++ ", " ++ show p6 ++ ", " ++ show p7
+  putStrLn $ show $ getDirection p5 p6 p7
+  putStrLn ""
+  putStrLn "That's it. Goodbye!"
