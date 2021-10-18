18c1e53ce156cefb2a7c38c758ce0fe8b4980037
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-10-18 15:06:12 +0200

    dodanie funkcji pomocniczych minX, minYthenX, minimumYthenX

--- book3/ch03/grahamScan.hs
+++ book3/ch03/grahamScan.hs
@@ -50,3 +50,15 @@ p7 = Point (2, 3)
 
 points :: [Point]
 points = [p1, p2, p3, p4, p5, p6, p7]
+
+minX :: Point -> Point -> Point
+minX (Point (x1, y1)) (Point (x2, y2)) =
+  if x1 < x2 then (Point (x1, y1)) else (Point (x2, y2))
+
+minYthenX :: Point -> Point -> Point
+minYthenX (Point (x1, y1)) (Point (x2, y2)) =
+  if y1 == y2 then minX (Point (x1, y1)) (Point (x2, y2))
+  else if y1 < y2 then (Point (x1, y1)) else (Point (x2, y2))
+
+minimumYthenX :: [Point] -> Point
+minimumYthenX pts = foldl1 (\acc curVal -> minYthenX acc curVal) pts
