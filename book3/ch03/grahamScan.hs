e7fdc266dda07ca816a652ffad5ef708fee35f05
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-10-27 22:19:27 +0200

    dod. fn-s licz. katy miedzy punktami, porownujace, sortujace putnkty

--- book3/ch03/grahamScan.hs
+++ book3/ch03/grahamScan.hs
@@ -1,7 +1,9 @@
+import Data.List (sortBy)
+
 type X = Double
 type Y = Double
 
-data Point = Point X Y
+data Point = Point X Y deriving (Eq)
 
 instance Show Point where
   show (Point x y) = "(" ++ show x ++ ", " ++ show y ++ ")"
@@ -30,6 +32,18 @@ p7 = Point 2 3
 points :: [Point]
 points = [p1, p2, p3, p4, p5, p6, p7]
 
+getAngleRadians :: Point -> Point -> Double
+getAngleRadians (Point x1 y1) (Point x2 y2) = atan2 (y2 - y1) (x2 - x1)
+
+ptsCompare :: Point -> Point -> Point -> Ordering
+ptsCompare p p1 p2 =
+  let angle1 = getAngleRadians p p1
+      angle2 = getAngleRadians p p2
+  in compare angle1 angle2
+
+ptsCompareWithP :: Point -> Point -> Ordering
+ptsCompareWithP = ptsCompare $ minimumYthenX points
+
 minX :: Point -> Point -> Point
 minX (Point x1 y1) (Point x2 y2) =
   if x1 < x2 then (Point x1 y1) else (Point x2 y2)
@@ -41,3 +55,9 @@ minYthenX (Point x1 y1) (Point x2 y2) =
 
 minimumYthenX :: [Point] -> Point
 minimumYthenX pts = foldl1 (\acc curVal -> minYthenX acc curVal) pts
+
+sortedRemPts :: [Point] -> [Point]
+sortedRemPts pts =
+  let ptP = minimumYthenX pts
+      ptsWithoutP = filter (\p -> p /= ptP) pts
+  in sortBy ptsCompareWithP ptsWithoutP
