f0292067ed21cddf91fbfc67d06b485dc2e63616
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-11-12 18:36:17 +0100

    dodanie zapisywania character-ow w JSON (unicode/hex)

--- book3/ch05/jsonUtils/PrettyStub.hs
+++ book3/ch05/jsonUtils/PrettyStub.hs
@@ -5,6 +5,17 @@ import SimpleJSON
 data Doc = ToBeDefined
          deriving (Show)
 
+-- equivalent for (++) for strings
+(<>) :: Doc -> Doc -> Doc
+a <> b = undefined
+
+char :: Char -> Doc
+char c = undefined
+
+-- analogue of concat for lists
+hcat :: [Doc] -> Doc
+hcat xs = undefined
+
 string :: String -> Doc
 string str = undefined
 
