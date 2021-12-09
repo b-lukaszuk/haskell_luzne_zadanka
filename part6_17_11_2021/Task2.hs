6b5c1f65a6d1a597fd6690b95f42574059257e83
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-12-09 20:45:17 +0100

    kosmetyka, getRndCard -> getRndCards

--- part6_17_11_2021/Task2.hs
+++ part6_17_11_2021/Task2.hs
@@ -11,15 +11,15 @@ prisoners = [0..9]
 noOfGuessesPerPris :: Int
 noOfGuessesPerPris = 8
 
-getRndCard :: IO [Card]
-getRndCard = do
+getRndCards :: IO [Card]
+getRndCards = do
   gen <- getStdGen
   _ <- newStdGen
   return $ take noOfGuessesPerPris $ randomRs (0, length(prisoners) - 1) gen
 
 -- now use, e.g:
--- fmap (anyCardEqlPrisId (prisoners !! 0)) getRndCard
--- to apply getRndCard to anyCardEqlPrisId
+-- fmap (anyCardEqlPrisId (prisoners !! 0)) getRndCards
+-- to apply getRndCards to anyCardEqlPrisId
 -- but it returns IO Bool
 anyCardEqlPrisId :: Int -> [Card] -> Bool
 anyCardEqlPrisId _ [] = False
