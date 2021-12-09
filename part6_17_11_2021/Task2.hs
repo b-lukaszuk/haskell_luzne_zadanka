ac455d1ca7fe638ce2ebcf5bb7f3813f4a25a03a
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-12-09 22:04:50 +0100

    dodano liczenie prawdopodobienstwa metoda losowa

--- part6_17_11_2021/Task2.hs
+++ part6_17_11_2021/Task2.hs
@@ -1,6 +1,7 @@
 module Task2 where
 
 import System.Random
+import Text.Printf (printf)
 
 type Prisoner = Int
 type Card = Int
@@ -10,6 +11,9 @@ prisoners = [0..9]
 
 noOfGuessesPerPris :: Int
 noOfGuessesPerPris = 8
+
+noOfIter :: Integer
+noOfIter = 10000
 
 getRndCards :: IO [Card]
 getRndCards = do
@@ -33,7 +37,27 @@ didAllPrisFoundLuckyCard (result:results) = do
   res <- result
   if not res then return False else didAllPrisFoundLuckyCard results
 
+runNIterRand :: Integer -> [IO Bool]
+runNIterRand 0 = []
+runNIterRand n = didAllPrisFoundLuckyCard (makePrisLookForLuckyCard prisoners) :
+  runNIterRand (n-1)
 
+calcProb :: Integer -> Integer -> [IO Bool] -> IO Double
+calcProb noOfSuc total [] = return ((fromIntegral noOfSuc) / (fromIntegral total))
+calcProb noOfSuc total (b:bs) = do
+  res <- b
+  if res
+    then calcProb (noOfSuc + 1) (total + 1) bs
+    else calcProb noOfSuc (total + 1) bs
+
+-- interesting,
+-- calculation of probability in Haskell's REPL is faster than in
+-- Python's REPL, ofcourse the results are similar
 main :: IO ()
 main = do
-  putStrLn "sth"
+    putStrLn "Calculating probability of success for:"
+    putStrLn "10 prisoners, 10 cards, 8 guesses each,"
+    putStrLn "strategy: random, iterations: 10k"
+    putStrLn "Please be patient, this may take a while"
+    probRand <- calcProb 0 0 $runNIterRand noOfIter
+    printf "p = %.5f\n" $ probRand
