cb5b1442f47f944e64698ca63083d946cd5650c9
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-01-15 21:27:17 +0100

    dodano rozwiazanie Task1

---
+++ part7_15_01_2022/Task1.hs
@@ -0,0 +1,18 @@
+module Task1 where
+
+-- for non negative integers (args)
+-- for relatively small numbers, otherwise you should compile it
+-- otherwise it freezes the program
+ackr:: Integer -> Integer -> Integer
+ackr 0 n = n + 1
+ackr m 0 = ackr (m - 1) 1
+ackr m n = ackr (m - 1) $ ackr m (n - 1)
+
+main :: IO ()
+main = do
+  putStrLn "Program for calculating Ackermann function"
+  putStrLn "IT MAY FREEZE THE COMPUTER!, or rather the program, e.g."
+  putStrLn "ackr 4 2, is an integer of 19,729 decimal digits"
+  putStrLn "For bigger args, like: ackr 4 2, please compile first"
+  putStrLn $ "ackr 3 0 = " ++ (show $ ackr 3 0) -- that one is safe
+  putStrLn $ "ackr 3 2 = " ++ (show $ ackr 3 2) -- that one as well
