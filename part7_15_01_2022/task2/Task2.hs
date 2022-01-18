c7410dc9874b5f64a397e142909b248894a6ad93
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-01-18 21:42:02 +0100

    pocztakowy commit + kilka funkcji pomocniczych

---
+++ part7_15_01_2022/task2/Task2.hs
@@ -0,0 +1,18 @@
+module Task2 where
+import Data.List.Split (splitOn)
+
+x = "Given$a$text$file$of$many$lines,$where$fields$within$a$line$ \nare$delineated$by$a$single$'dollar'$character,$write$a$program \nthat$aligns$each$column$of$fields$by$ensuring$that$words$in$each$ \ncolumn$are$separated$by$at$least$one$space. \nFurther,$allow$for$each$word$in$a$column$to$be$either$left$ \njustified,$right$justified,$or$center$justified$within$its$column."
+
+lpad :: Int -> String -> String -> String
+lpad 0 text _ = text
+lpad finalLen text lPad = if (length text) > finalLen then text
+                          else lpad (finalLen - 1) (lPad ++ text) lPad
+
+getWords :: String -> [[String]]
+getWords someTxt = map (splitOn "$") $ lines someTxt
+
+getWordsLens :: [[String]] -> [[Int]]
+getWordsLens wrds = map (map length) $ wrds
+
+getNoOfColsInRow :: [[Int]] -> [Int]
+getNoOfColsInRow wordsLens = map length wordsLens
