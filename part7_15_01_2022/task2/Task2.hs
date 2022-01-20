d71f743a3702c7de534bbf705e1b5601d770dd35
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-01-20 19:14:14 +0100

    kosmetyka

--- part7_15_01_2022/task2/Task2.hs
+++ part7_15_01_2022/task2/Task2.hs
@@ -3,10 +3,10 @@ import Data.List.Split (splitOn)
 
 x = "Given$a$text$file$of$many$lines,$where$fields$within$a$line$ \nare$delineated$by$a$single$'dollar'$character,$write$a$program \nthat$aligns$each$column$of$fields$by$ensuring$that$words$in$each$ \ncolumn$are$separated$by$at$least$one$space. \nFurther,$allow$for$each$word$in$a$column$to$be$either$left$ \njustified,$right$justified,$or$center$justified$within$its$column."
 
-lpad :: Int -> String -> String -> String
-lpad 0 text _ = text
-lpad finalLen text lPad = if (length text) > finalLen then text
-                          else lpad (finalLen - 1) (lPad ++ text) lPad
+lpad :: String -> Int -> String -> String
+lpad _ 0 text = text
+lpad lPad finalLen text = if (length text) >= finalLen then text
+                          else lpad lPad(finalLen - 1) (lPad ++ text)
 
 getWords :: String -> String -> [[String]]
 getWords sep someTxt = map (splitOn sep) $ lines someTxt
