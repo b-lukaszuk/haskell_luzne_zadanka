175e5c70089652fb0c14ebdbc34a99b40631e934
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-02-01 21:05:24 +0100

    dod. getMostAnagrams + helpery getAnagramsOfWord i remAnagramsOfWord

--- part7_15_01_2022/task3/Task3.hs
+++ part7_15_01_2022/task3/Task3.hs
@@ -11,3 +11,18 @@ isAnagram word1 word2 =
   let w1 = toLowerAndSort word1
       w2 = toLowerAndSort word2
   in w1 == w2
+
+getAnagramsOfWord :: String -> [String] -> [String]
+getAnagramsOfWord aWord dict = filter (\item -> isAnagram aWord item) dict
+
+remAnagramsOfWord :: String -> [String] -> [String]
+remAnagramsOfWord aWord dict = filter (\item -> not $ isAnagram aWord item) dict
+
+getMostAnagrams :: [String] -> [String] -> [String]
+getMostAnagrams curMost [] = curMost
+getMostAnagrams curMost dict =
+  let newAnagrams = getAnagramsOfWord (head dict) dict
+      newDict = remAnagramsOfWord (head dict) dict
+  in if (length newAnagrams) > (length curMost)
+     then getMostAnagrams newAnagrams newDict
+     else getMostAnagrams curMost newDict
