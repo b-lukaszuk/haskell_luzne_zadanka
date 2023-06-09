97c0306946ac8dfe5084d7e08e022e8e666241ea
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2023-06-09 20:26:09 +0200

    added task description/solution to ws1/p3/

---
+++ ws1/p3/p3.hs
@@ -0,0 +1,21 @@
+getKthElt :: Int -> [a] -> Maybe a
+getKthElt _ [] = Nothing
+getKthElt k (x:xs)
+  | k <= 0 = Nothing
+  | k == 1 = Just x
+  | otherwise = getKthElt (k - 1) xs
+
+getList :: Int -> [Int]
+getList len
+  | len <= 0 = []
+  | otherwise = [1 .. len]
+
+getExampleInfo :: (Show a) => Int -> [a] -> String
+getExampleInfo k l =
+  show l ++ " !! " ++ show k ++ " => " ++ show (getKthElt k l)
+
+main :: IO ()
+main = do
+  let ls = map getList [0 .. 5]
+  print "Getting last two element of a list examples"
+  mapM_ (print . getExampleInfo 3) ls
