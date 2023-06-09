82aa18362e18fc15cf0690dafa23b531f2720e3a
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2023-06-09 22:59:23 +0200

    added task description/solution to ws1/p5/

---
+++ ws1/p5/README.md
@@ -0,0 +1,18 @@
+# Info
+
+Solving some tasks in Haskell 'at a whim'.
+
+The tasks are from [99 Problems (solved) in OCaml](https://v2.ocaml.org/learn/tutorials/99problems.html) solved with Haskell [to give it (the language and functional programming) another try].
+
+# Problem
+
+Reverse a list. (easy)
+
+# Additional info
+
+Taken/solved for personal and educational use only.
+
+**The content of this folder may be incorrect, erroneous and/or harmful. Use it at Your own risk.**
+
+**Zawartość niniejszego katalogu może być nieprawidłowa, błędna czy szkodliwa. Używaj na własne ryzyko.**
+
---
+++ ws1/p5/p5.hs
@@ -0,0 +1,31 @@
+-------------------------------------------------------------------------------
+--                      classical version with recursion                     --
+-------------------------------------------------------------------------------
+-- revList' :: [a] -> [a] -> [a]
+-- revList' [] acc     = acc
+-- revList' (x:xs) acc = revList' xs (x : acc)
+
+-- revList :: [a] -> [a]
+-- revList l = revList' l []
+
+-------------------------------------------------------------------------------
+--                             version with foldl                            --
+-------------------------------------------------------------------------------
+revList2 :: [a] -> [a]
+-- the code below is shorter equivalent to
+-- revList2 = foldl (\acc cur -> cur : acc) []
+revList2 = foldl (flip (:)) []
+
+getList :: Int -> [Int]
+getList len
+  | len <= 0 = []
+  | otherwise = [1 .. len]
+
+getExampleInfo :: (Show a) => [a] -> String
+getExampleInfo l = show l ++ " => " ++ show (revList2 l)
+
+main :: IO ()
+main = do
+  let ls = map getList [0 .. 5]
+  print "Reversing a list examples"
+  mapM_ (print . getExampleInfo) ls
