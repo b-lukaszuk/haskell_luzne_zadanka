8899b26c91d66e64cf5446182a43c76093568984
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2023-06-09 20:10:02 +0200

    added task description/solution to ws1/p2/

---
+++ ws1/p2/p2.hs
@@ -0,0 +1,18 @@
+getLastTwo :: [a] -> Maybe [a]
+getLastTwo []     = Nothing
+getLastTwo [x, y] = Just [x, y]
+getLastTwo (_:xs) = getLastTwo xs
+
+getList :: Int -> [Int]
+getList len
+  | len <= 0 = []
+  | otherwise = [1 .. len]
+
+getExampleInfo :: (Show a) => [a] -> String
+getExampleInfo l = show l ++ " => " ++ show (getLastTwo l)
+
+main :: IO ()
+main = do
+  let ls = map getList [0 .. 5]
+  print "Getting last two element of a list examples"
+  mapM_ (print . getExampleInfo) ls
