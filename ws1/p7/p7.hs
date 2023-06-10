2f95dcffffe512a9a485950b5e834e44b218cd5d
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2023-06-10 20:39:37 +0200

    added task description/solution to pws1/p7/

---
+++ ws1/p7/p7.hs
@@ -0,0 +1,23 @@
+-------------------------------------------------------------------------------
+--                   flattens list of lists with recursion                   --
+-------------------------------------------------------------------------------
+-- flattenList :: [[a]] -> [a]
+-- flattenList []     = []
+-- flattenList (x:xs) = x ++ flattenList xs
+
+-------------------------------------------------------------------------------
+--                   flattens list of lists with foldr                       --
+-------------------------------------------------------------------------------
+flattenList :: [[a]] -> [a]
+flattenList = foldr (++) []
+
+getExampleInfo :: (Show a) => [[a]] -> String
+getExampleInfo lol = (show lol) ++ " => " ++ show (flattenList lol)
+
+main :: IO ()
+main = do
+  let lol1 = [[1..3], [4..6], [7..10]] :: [[Int]]
+  let lol2 = ["one, ", "two, ", "three"]
+  print "Flattening list of lists example"
+  print $ getExampleInfo lol1
+  print $ getExampleInfo lol2
