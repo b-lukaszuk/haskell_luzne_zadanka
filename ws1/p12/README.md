9d794c57ebba3d0a820580abd0d4fea6b474f9f8
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2023-06-14 19:50:19 +0200

    added task description/solution to ws1/p12/

---
+++ ws1/p12/README.md
@@ -0,0 +1,26 @@
+# Info
+
+Solving some tasks in Haskell 'at a whim'.
+
+The tasks are from [99 Problems (solved) in OCaml](https://v2.ocaml.org/learn/tutorials/99problems.html) solved with Haskell [to give it (the language and functional programming) another try].
+
+# Problem
+
+Decode a run-length encoded list. (medium)
+
+Given a run-length code list generated as specified in the previous problem, construct its uncompressed version.
+
+<pre>
+# decode [Many (4, "a"); One "b"; Many (2, "c"); Many (2, "a"); One "d"; Many (4, "e")];;
+- : string list =
+["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"] </pre>
+</pre>
+
+# Additional info
+
+Taken/solved for personal and educational use only.
+
+**The content of this folder may be incorrect, erroneous and/or harmful. Use it at Your own risk.**
+
+**Zawartość niniejszego katalogu może być nieprawidłowa, błędna czy szkodliwa. Używaj na własne ryzyko.**
+
---
+++ ws1/p12/p12.hs
@@ -0,0 +1,40 @@
+data OneMany a
+  = One a
+  | Many
+      { count :: Int
+      , val   :: a
+      }
+  deriving (Show)
+
+ungroup :: OneMany a -> [a]
+ungroup (One x)                   = [x]
+ungroup Many {count = c, val = v} = replicate c v
+
+decodeRunLength :: [OneMany a] -> [a]
+decodeRunLength = foldr (\cur acc -> ungroup cur ++ acc) []
+
+getExampleInfo :: (Show a) => [OneMany a] -> String
+getExampleInfo l = show l ++ " =>\n" ++ show (decodeRunLength l)
+
+main :: IO ()
+main = do
+  let l1 =
+        [ Many {count = 3, val = True}
+        , Many {count = 2, val = False}
+        , One True
+        , Many {count = 4, val = False}
+        ]
+  let l2 =
+        [ One 1
+        , Many {count = 2, val = 2}
+        , One 3
+        , Many {count = 2, val = 4}
+        , One 5
+        , Many {count = 3, val = 6}
+        , Many {count = 2, val = 7}
+        ] :: [OneMany Int]
+  putStrLn "Modified run-length encoding of a list."
+  putStrLn "Examples:\n"
+  putStrLn $ getExampleInfo l1
+  putStrLn $ "\n" ++ getExampleInfo l2
+  putStrLn "\nThat's all. Goodbye!"
