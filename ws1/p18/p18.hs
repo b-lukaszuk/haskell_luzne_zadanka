3b66a9dd468360f1fa82c23794f52da8f7d35c7d
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2023-06-27 21:27:55 +0200

    added solution to ws1/p18/

---
+++ ws1/p18/p18.hs
@@ -0,0 +1,20 @@
+-- indexing starts at 0, sclicing is start - end (inclusive - inclusive)
+-- start must be >= 0, end >= start
+slice :: [a] -> Int -> Int -> [a]
+slice xs start end = drop start $ take (end + 1) xs
+
+getExampleInfo :: (Show a) => [a] -> Int -> Int -> String
+getExampleInfo l s e =
+  show l ++ " (" ++ show s ++ ":" ++ show e ++ ") => " ++ show (slice l s e)
+
+main :: IO ()
+main = do
+  putStrLn "Slicing lists (0 based indexing, inclusing-inclusing)."
+  putStrLn "Examples:\n"
+  let l1 = ['a'..'j']
+  let l2 = [1..12]
+  let l3 = [1..4]
+  putStrLn $ getExampleInfo l1 2 6
+  putStrLn $ getExampleInfo l2 0 4
+  putStrLn $ getExampleInfo l3 2 7
+  putStrLn "\nThat's all. Goodbye!"
