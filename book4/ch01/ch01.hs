52333daf957ca73ba3f545c9eec44f13b30dbe73
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2023-12-22 11:35:59 +0100

    added code for ch01/1.1.6 Cond. Expr. and Predicates

--- book4/ch01/ch01.hs
+++ book4/ch01/ch01.hs
@@ -14,3 +14,22 @@ sumOfSquares x y = (square x) + (square y)
 
 f :: Num a => a -> a
 f a = sumOfSquares (a + 1) (a * 2)
+
+-- Conditional Expressions and Predictates
+myAbs :: (Ord a, Num a) => a -> a
+myAbs x
+  | x > 0 = x
+  | x == 0 = 0
+  | otherwise = negate x
+
+myAbs' :: (Ord a, Num a) => a -> a
+myAbs' x = if (x < 0) then negate x else x
+
+-- Ex 1.3
+-- Define a procedure that takes three numbers as arguments and
+-- returns the sum of the squares of the two larger numbers.
+getSumOfSquaresOfBigNums :: (Ord a, Num a) => a -> a -> a -> a
+getSumOfSquaresOfBigNums x y z
+  | (x < y) && (x < z) = sumOfSquares y z
+  | (y < x) && (y < z) = sumOfSquares x z
+  | otherwise = sumOfSquares x y
