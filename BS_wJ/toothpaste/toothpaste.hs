e038e00bdf9b3f598841ec8cab5de0f7eb0ea1ba
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2025-10-09 23:43:22 +0200

    added BS_wJ/toothpaste/toothpaste.hs

---
+++ BS_wJ/toothpaste/toothpaste.hs
@@ -0,0 +1,42 @@
+data Cylinder = Cylinder {radius  :: Double
+                         , height :: Double
+                         } deriving (Show)
+
+
+getVolume :: Cylinder -> Double
+getVolume c = height c * pi * (radius c)^2
+
+round' :: Double -> Integer -> Double
+round' num precision = (fromIntegral . round $ num * f) / f
+  where f = 10^precision
+
+round2 :: Double -> Double
+round2 num = round' num 2
+
+getChangedCylinder :: Double -> Double -> Cylinder -> Cylinder
+getChangedCylinder dr dh c = Cylinder (radius c + dr) $ height c + dh
+
+getChangedCylinders :: Double -> Double -> [Cylinder] -> [Cylinder]
+getChangedCylinders dr dh cs = map (getChangedCylinder dr dh) cs
+
+getVolumeRatio :: Cylinder -> Cylinder -> Double
+getVolumeRatio c1 c2 = getVolume c2 / getVolume c1
+
+getVolumeRatios :: [Cylinder] -> [Cylinder] -> [Double]
+getVolumeRatios cs1 cs2 = map round2 ratios
+  where ratios = zipWith getVolumeRatio cs1 cs2
+
+getRatiosAfterChange :: Double -> Double -> [Cylinder] -> [Double]
+getRatiosAfterChange dr dh cs = map (*100) $ getVolumeRatios cs cs2
+  where cs2 = getChangedCylinders dr dh cs
+
+-- uncomment the appropriate part, then copy-paste into REPL
+
+-- scenario 1
+-- getRatiosAfterChange 1 0 $ map (flip Cylinder 5) [1..5]
+
+-- scenario 2
+-- getRatiosAfterChange 1 (negate 1) $ map (flip Cylinder 5) [1..5]
+
+-- scenario 3
+-- getRatiosAfterChange 1 (negate 2) $ map (flip Cylinder 5) [1..5]
