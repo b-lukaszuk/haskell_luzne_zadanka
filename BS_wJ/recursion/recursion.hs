c5db3a00b85666b6bc2d55bbdb6892b9a7407e5c
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2025-10-13 19:38:09 +0200

    added part of solution to BS_wJ/recursion/recursion.hs

--- BS_wJ/recursion/recursion.hs
+++ BS_wJ/recursion/recursion.hs
@@ -6,3 +6,16 @@ recSum (x:xs) = x + recSum xs
 
 -- uncomment and type the below into the ghci (REPL)
 -- map recSum [[1], [1..3], [1..100]]
+
+-- short version: getFactorial n = product [1..n]
+getFactorial' :: Int -> Int -> Int
+getFactorial' n acc
+  | n < 1 = undefined
+  | n == 1 = acc
+  | otherwise = getFactorial' (n-1) (acc*n)
+
+getFactorial :: Int -> Int
+getFactorial n = getFactorial' n 1
+
+-- uncomment and type the below into the ghci (REPL)
+-- map getFactorial [1..10]
