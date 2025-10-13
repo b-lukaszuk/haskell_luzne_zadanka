c73a3e8c53ace1e3201ede96022f5bd91f01ee20
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2025-10-13 20:48:44 +0200

    cosmetic changes

--- BS_wJ/recursion/recursion.hs
+++ BS_wJ/recursion/recursion.hs
@@ -1,3 +1,5 @@
+import           Numeric.Natural
+
 -- from Section: 7.1 Problem
 
 recSum :: (Num a) => [a] -> a
@@ -7,27 +9,28 @@ recSum (x:xs) = x + recSum xs
 -- uncomment and type the below into the ghci (REPL)
 -- map recSum [[1], [1..3], [1..100]]
 
+
+-- from Section: 7.2 Solution
+
 -- short version: getFactorial n = product [1..n]
-getFactorial' :: Int -> Int -> Int
+getFactorial' :: Natural -> Natural -> Natural
 getFactorial' n acc
-  | n < 1 = undefined
-  | n == 1 = acc
+  | n <= 1 = acc
   | otherwise = getFactorial' (n-1) (acc*n)
 
-getFactorial :: Int -> Int
+getFactorial :: Natural -> Natural
 getFactorial n = getFactorial' n 1
 
 -- uncomment and type the below into the ghci (REPL)
 -- map getFactorial [1..10]
 
 
-getFib' :: Int -> Int -> Int -> Int
+getFib' :: Natural -> Natural -> Natural -> Natural
 getFib' a b n
-  | n < 0 = undefined
   | n < 1 = b
   | otherwise = getFib' b (b+a) (n-1)
 
-getFib :: Int -> Int
+getFib :: Natural -> Natural
 getFib n = getFib' 1 0 n
 
 -- uncomment and type the below into the ghci (REPL)
