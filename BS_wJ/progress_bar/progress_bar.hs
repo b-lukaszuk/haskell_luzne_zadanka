6a3b178c75b0ea26c5266cc35d4b291a7c1aad7d
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2025-10-16 22:58:48 +0200

    added part of solution to BS_wJ/progress_bar/progress_bar.hs

--- BS_wJ/progress_bar/progress_bar.hs
+++ BS_wJ/progress_bar/progress_bar.hs
@@ -11,15 +11,28 @@ getProgressBar perc fan
         q = maxNumOfChars - p
         qs = take q $ repeat '.'
 
+-- the terminal must support ANSI escape codes
+-- https://en.wikipedia.org/wiki/ANSI_escape_code
+clear1Line :: IO ()
+clear1Line = do
+    -- "\033[xxxA" - xxx moves cursor up xxx lines
+    -- in haskell you use hex code instead of octal, hence "\033" is "\x1b"
+    putStr "\x1b[A"
+    -- clears from cursor position till end of display
+    putStr "\x1b[J"
+
 animate1Frame :: Int -> String -> IO ()
 animate1Frame perc fan = do
-  print $ getProgressBar perc fan
-  threadDelay $ 200 * 1000
+  putStrLn $ getProgressBar perc fan
+  threadDelay $ 150 * 1000 -- delay: 150 ms
+  clear1Line
 
 fans :: [String]
 fans = ["\\", "-", "/", "-"]
 
+-- run program from shell with: runhaskell progress_bar.hs
 main :: IO()
-main = forM_ [0..100] $ \i -> do
-  animate1Frame i (fans !! (mod i $ length fans))
-
+main = do
+  forM_ [0..100] $ \i -> do
+    animate1Frame i (fans !! (mod i $ length fans))
+  putStrLn $ getProgressBar 100 (fans !! 0)
