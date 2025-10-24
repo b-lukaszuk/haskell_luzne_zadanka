c6f172ae6eb5b5b7bca1c8ed034635abeb97b6f7
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2025-10-24 18:51:19 +0200

    cosmetic changes in BS_wJ/bat_and_ball/bat_and_ball.hs

--- BS_wJ/bat_and_ball/bat_and_ball.hs
+++ BS_wJ/bat_and_ball/bat_and_ball.hs
@@ -11,18 +11,18 @@ instance {-# OVERLAPPING #-} Show Matrix where
   show m = mat2str m
 
 getCol :: Matrix -> Int -> [Double]
-getCol mat n = map (\row -> row !! n) mat
+getCol mat n = map (!! n) mat
 
 getCols' :: Matrix -> Int -> [[Double]]
 getCols' mat n
   | n < 0 = []
-  | otherwise = (getCol mat n) : getCols' mat (n-1)
+  | otherwise = getCol mat n : getCols' mat (n-1)
 
 getNCols :: Matrix -> Int
 getNCols m = length $ head m
 
 getCols :: Matrix -> [[Double]]
-getCols mat = reverse $ getCols' mat $ (getNCols mat - 1)
+getCols mat = reverse $ getCols' mat (getNCols mat - 1)
 
 getDotProd :: [Double] -> [Double] -> Double
 getDotProd row col = sum $ zipWith (*) row col
@@ -38,21 +38,21 @@ getValAtIndex m r c = (m !! r) !! c
 
 getDeterminant2x2 :: Matrix -> Double
 getDeterminant2x2 m =  firstPart - secondPart
-  where firstPart = (getValAtIndex m 0 0) * (getValAtIndex m 1 1)
-        secondPart = (getValAtIndex m 0 1) * (getValAtIndex m 1 0)
+  where firstPart = getValAtIndex m 0 0 * getValAtIndex m 1 1
+        secondPart = getValAtIndex m 0 1 * getValAtIndex m 1 0
 
 getDeterminant2x2Reciprocal :: Matrix -> Double
-getDeterminant2x2Reciprocal m = 1.0 / (getDeterminant2x2 m)
+getDeterminant2x2Reciprocal m = 1.0 / getDeterminant2x2 m
 
 getSwap :: Matrix -> Matrix
 getSwap m = [[a, b], [c, d]]
   where a = getValAtIndex m 1 1
-        b = (getValAtIndex m 0 1) * negate 1
-        c = (getValAtIndex m 1 0) * negate 1
-        d = (getValAtIndex m 0 0)
+        b = getValAtIndex m 0 1 * negate 1
+        c = getValAtIndex m 1 0 * negate 1
+        d = getValAtIndex m 0 0
 
 getInverse2x2 :: Matrix -> Matrix
-getInverse2x2 m = [[reciprocal * (getValAtIndex swap r c) |
+getInverse2x2 m = [[reciprocal * getValAtIndex swap r c |
                     c <- [0..nCols-1]] | r <- [0..nRows-1]]
   where reciprocal = getDeterminant2x2Reciprocal m
         swap = getSwap m
